35869610f71cb63fea14516619e97860e0966e84
lelopez <ux.lelopez@gmail.com>
2018-04-03 08:54:01 -0500

    Create PID Prototypes

---
+++ pid_controller.c
@@ -0,0 +1,10 @@
+/*
+ * pid_controller.c
+ *
+ *  Created on: Apr 3, 2018
+ *      Author: lelopez
+ */
+
+
+
+
---
+++ pid_controller.h
@@ -0,0 +1,16 @@
+/*
+ * pid_controller.h
+ *
+ *  Created on: Apr 3, 2018
+ *      Author: lelopez
+ */
+
+#ifndef PID_CONTROLLER_H_
+#define PID_CONTROLLER_H_
+
+void PIDInit();
+void PIDCompute();
+
+
+
+#endif /* PID_CONTROLLER_H_ */
